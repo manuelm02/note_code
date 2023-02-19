0546b10e7ba5e55ac1dbe432d5f92cef41351753
manuelm02 <1017208891@qq.com>
2023-02-19 21:35:34 +0800

    build note code project

---
+++ src/main/java/com/manuel/note_code/NoteCodeApplication.java
@@ -0,0 +1,16 @@
+package com.manuel.note_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: manuel
+ * @Date: Created in 2023/2/19 21:22
+ * @Description: 启动项入口
+ */
+@SpringBootApplication
+public class NoteCodeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NoteCodeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/manuel/note_code/NoteCodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.manuel.note_code;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author: manuel
+ * @Date: Created in 2023/2/19 21:24
+ * @Description: 启动入口测试类
+ */
+@SpringBootTest
+public class NoteCodeApplicationTests {
+
+	@Test
+	public void contextLoads() {}
+}
