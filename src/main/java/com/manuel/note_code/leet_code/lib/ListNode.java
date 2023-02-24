89b00426a532af7d952e9664a282eeb830a52dd0
manuelm02 <1017208891@qq.com>
2023-02-24 21:32:50 +0800

    daily code

---
+++ src/main/java/com/manuel/note_code/leet_code/lib/ListNode.java
@@ -0,0 +1,20 @@
+package com.manuel.note_code.leet_code.lib;
+
+import lombok.Data;
+
+/**
+ * @Author: manuel
+ * @Date: Created in 2023/2/24 21:27
+ * @Description: 链表节点
+ */
+@Data
+public class ListNode {
+
+  public int val;
+  public ListNode next;
+
+  ListNode(int x) {
+    val = x;
+    next = null;
+  }
+}
